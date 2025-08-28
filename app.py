cf71f806c6f281440d5ecb414fba107344bc878f
carloag210 <carloag210@hotmail.com>
2025-08-28 16:45:31 -0500

    commit:api key

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'logistica@porencargo.co'         # tu correo
-app.config['MAIL_PASSWORD'] = 'pm-live-c0196312-75cd-4eb2-a8bd-5085bd590a43'           # contraseña de aplicación
+app.config['MAIL_PASSWORD'] = 'pm-live-1711d7f7-4e24-4425-9a6d-a912c1ddbc0c'           # contraseña de aplicación
 app.config['MAIL_DEFAULT_SENDER'] = 'logistica@porencargo.co'   # quien envía
     
 mail = Mail(app)
